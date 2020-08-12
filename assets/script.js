2b566a4b17fe1971c59d6dd4aec2bf0313df269c
asxyzp <asxyzp@gmail.com>
2020-08-12 15:32:11 +0000

    Improvements w/ notification tab

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,33 @@
-//Will be storing elements from which a 7-digit pseudo random character will be generated
-let randomCharArr = [];
-let randomNumArr = [];
+//Client-side script for rttt to provide interactivity & send/get requests
+//Created by asxyzp (Aashish Loknath Panigrahi)
 
-//Adding 0-9 & A-Z to the randomArrElement
-//String.fromCharCode() converts number to string 
-for(let i=0;i<=9;i++)
-	randomNumArr.push(i);
-for(let i=65;i<=65+25;i++)
-	randomCharArr.push(String.fromCharCode(i));
+let randomCharArr = [];		//Stores A-Z
+let randomNumArr = [];		//Stores 0-9
 
-//Will store 7 digit random string
+/*
+	Function : generateArrElements()
+	Functionality : Adds elements to the arrays randomCharArr & randomNumArr
+	Parameters : NONE
+	Return value : NONE
+*/
+let generateArrElements = () =>{
+	//Adding 0-9 & A-Z to the randomNumArr & randomCharArr respectively
+	//String.fromCharCode() converts number to string 
+	for(let i=0;i<=9;i++)
+		randomNumArr.push(i);
+	for(let i=65;i<=65+25;i++)
+		randomCharArr.push(String.fromCharCode(i));
+}
+generateArrElements();
+
+/*
+	Function : generateRandomID()
+	Functionality : Generates 7-digit random ID for the game
+	Parameters : NONE
+	Return value : SevenDigitRandomString -  A seven digit random string
+*/
 let generateRandomID = () => {
-	let SevenDigitRandomString ='';
+	let SevenDigitRandomString ='';		//Stores the seven digit pseudo-random ID
 	for(let i=0;i<4;i++)
 		SevenDigitRandomString+=randomCharArr[Math.floor(Math.random()*randomCharArr.length)];
 	for(let i=0;i<3;i++)
@@ -22,46 +38,57 @@ let generateRandomID = () => {
 
 //Performing DOM operations using jQuery
 $(document).ready(function(){
+	//Message which shows temporary ID is hidden by default
 	$('.tempID-msg').hide();
 	
-	//To handle request to the server when the Generate Random ID button is clicked & to show the generates random ID
+	//To handle request to the server when the "Generate Random ID" button is clicked & to show it generates random ID
 	$('.tempID-button').click(function(){
-		//Generates & stores randomly generated ID everytime when the "Generate Random ID" button is clicked
+		//Stores randomly generated ID everytime when the "Generate Random ID" button is clicked
 		let randomID = generateRandomID();
 
 		//Copies TenDigitRandomString to the clipboard
 		navigator.clipboard.writeText(randomID);	
 
-		$('.tempID-msg').show();
-		$('.tempID-show').text(randomID);
-		$('.send-request-form').css('display','flex');
-		
-		//Gets timestamp to be stored in the server
-		let date = new Date();
-		let timeStamp = date.getTime();
+		$('.tempID-msg').show();			//Message which shows temporary ID is shown.
+		$('.tempID-show').text(randomID);	//Box shows the temporary ID
+		$('.send-request-form').css('display','flex');	//Form for sending game request is also shown when the button is clicked
 
 		//Sending request to server that a new user has been added
-		fetch("/newUser?="+randomID+'&timeStamp?='+timeStamp+'/')
+		//Response could either be ERROR or SUCCESS
+		fetch("/newUser?="+randomID+'/')
 		.then(response=>response.text())
 		.then(msg=>{
-			//When the user has successfully created a temporary ID then a message will be sent.
+
+			//successTxt stores stores the text in success message.
+			let successTxt =`🙌 Yass! You're now connected to the game server using temporary ID <strong>${randomID}</strong>.`;
+
+			//successMsg is the message which will be displayed when a new temporary ID has been successfully generated 
+			let successMsg =`
+			<div class='alert alert-success' id='alert-server-connect-success' role='alert'>
+				<span class='alert-text'>${successTxt}</span>
+			</div>`;
+			
+			//Error text is the text in the error message
+			let errorTxt = `😓 Whoops! You can't connect to the game server. Try again.`;
+			
+			//Error message contains the HTML tags which will be displayed when response is 'ERROR'
+			let errorMsg = `
+					<div class='alert alert-danger' id='alert-server-connect-error' role='alert'>
+						<span class='alert-text'>
+							${errorTxt}
+						</span>
+					</div>`;
+
+			//When the user has successfully created a temporary ID then a message will be sent
 			if(msg=="SUCCESS"){
-				$(".notification-box").append("<div class='alert alert-success' id='alert-server-connect-success' role='alert'><span class='alert-text'>Yass! You're connected to the server.</span><span class='alert-dismiss'><button type='button' class='close' id='close-success' aria-label='Close'><span aria-hidden='true'>&times;</span></button></span></div>");
+				console.log(msg);
+				$(".notification-box").append(successMsg);
 			}
 			else{
-				$(".notification-box").append("YASS");
+				console.log(msg);
+				$(".notification-box").append(errorMsg);
 			}
 		});
-	});
-
-	//To close the messages in the notification box
-	//To close the alert success box when conncected to the server
-	$("#close-success").click(function(){
-		$("#alert-server-connect-success").remove();
-	});
-	//To close the alert error box when conncected to the server
-	$("#close-error").click(function(){
-		$("#alert-server-connect-error").remove();
 	});
 
 	//To change the text in the notfication toggle button
@@ -69,14 +96,17 @@ $(document).ready(function(){
 		$("#notification-btn").html($(".notification-box").children().length);
 		//Changing the notification box color based on the number of notifications
 		//When notification>0 then color of btn is red
+		//No notification text will also be displayed depending on this count
 		if($(".notification-box").children().length!=0){			 
 			$("#notification-btn").removeClass("btn-primary");
 			$("#notification-btn").addClass("btn-danger");
+			$(".no-notification-txt").css('display','none');
 		}
 		//Otherwise the color of notification btn is blue 
 		else{
 			$("#notification-btn").removeClass("btn-danger");
 			$("#notification-btn").addClass("btn-primary");
+			$(".no-notification-txt").css('display','block');
 		}
 	},100);
 	
--- index.js
+++ index.js
@@ -3,15 +3,19 @@
 let http = require('http');
 let fs = require('fs');
 
+//users object will store information about all the objects 
+let users = {};
+
 http.createServer((req,res)=>{
-	//assets object will store location of HTML, CSS & JS file which will be sent as responses
+	//assets object will store location of HTML, CSS & JS file & other objects which will be sent as responses
 	let assets={
 		LOGO:"assets/rttt.png",
 		HTML:"assets/index.html",
 		CSS :"assets/style.css",
 		JS  :"assets/script.js"
 	}
-	let users = {};
+	
+	//When the browser requests the main (index.html) page
 	if(req.url=="/"){
 		fs.readFile(assets.HTML,function(err,HTML){
 			if(err){
@@ -25,6 +29,8 @@ http.createServer((req,res)=>{
 			return console.log('HTML file sent');
 		});
 	}
+
+	//When the browser requests CSS file for the main page
 	if(req.url=="/style.css"){
 		fs.readFile(assets.CSS,function(err,CSS){
 			if(err){
@@ -37,6 +43,8 @@ http.createServer((req,res)=>{
 			return console.log('CSS file sent');
 		});
 	}
+
+	//When the browser requests JS file for the main page
 	if(req.url=='/script.js'){
 		fs.readFile(assets.JS,function(err,JS){
 			if(err){
@@ -49,6 +57,8 @@ http.createServer((req,res)=>{
 			return console.log('JS file sent');
 		});
 	}
+
+	//When the user requests for the application LOGO
 	if(req.url=='/rttt.png'){
 		fs.readFile(assets.LOGO,function(err,LOGO){
 			if(err){
@@ -61,9 +71,65 @@ http.createServer((req,res)=>{
 			return console.log('LOGO image sent');
 		});
 	}
-	if(req.url.indexOf("/newUser?=")!=-1){
-		if(req.url.indexOf("=")==1){
 
+	//Handling all requests related to the game, including adding the new user ID to the users object
+	if(req.url.indexOf("/newUser?=")!=-1){
+		let newUserID = req.url.substring(
+			req.url.indexOf("/newUser?=")+"/newUser?=".length,
+			req.url.indexOf("/newUser?=")+"/newUser?=".length+7
+		);
+		if(newUserID in users == false){
+			//Gets timestamp to be stored in the server
+			let date = new Date();
+			let timeStamp = date.getTime();
+
+			//Storing the userID w/ timestamp in the users object
+			users[newUserID]=timeStamp;
+			
+			//Sending response
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write("SUCCESS");
+			res.end();
+			console.log("All users : ",users);
+			return console.log('new user '+newUserID+' generated at '+timeStamp);
+		}
+
+		//For handling timeout
+		if(req.url.indexOf("/timeout?=")!=-1){
+
+			//Extracting user ID
+			let userID = req.url.substring(
+				req.url.indexOf("/timeout?=")+"/timeout?=".length,
+				req.url.indexOf("/timeout?=")+"/timeout?=".length+7
+			);
+
+			//Checking if userID exists in users object
+			//If yes then userID is deleted from the users object
+			if(userID in users == true){
+				delete users[userID];	//Delete userID from the user object
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				res.write("SUCCESS");	//Returning successful timeout response
+				res.end();
+
+				//Displaying information in console
+				console.log("All users : ",users);
+				return console.log('User '+userID+' timed out.');
+			}
+			//Else if the user ID is not in users object then ERROR msg. is sent
+			else{
+				res.writeHead(200, {'Content-Type': 'text/plain'});
+				res.write("ERROR");
+				res.end();
+				return console.log('Whoops! '+userID+" is not in users object.");
+			}
+		}
+
+		//Default
+		else{
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write("ERROR");
+			res.end();
+			return console.log('Whoops!');
 		}
 	}	
 }).listen(5000);
